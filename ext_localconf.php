945fa872bed894c95772315c96bf95efa5f72a6b
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-18 11:07:09 +0000

    Reformated call to registerTypoScript()
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mvc_extjs_samples/trunk@22440 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,14 +3,11 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+	// Plugin registration (frontend rendering)
 Tx_Extbase_Utility_Plugin::registerTypoScript(
 	'MvcExtjsSamples',
 	'Pi1',
-	array( 
-		'HelloWorld' => 'index', 
-		),
-	array(
-		
-		)
+	array('HelloWorld' => 'index'),
+	array()
 );
 ?>
