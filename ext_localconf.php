2cde0f693e2a6db3fabce772909c3f480444be87
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-19 12:35:54 +0000

    make it compatible with extbase r968
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mvc_extjs_samples/trunk@22488 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,28 +4,28 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 	// Plugin registration (frontend rendering)
-Tx_Extbase_Utility_Plugin::registerTypoScript(
+Tx_Extbase_Utility_Plugin::configureDispatcher(
 	'MvcExtjsSamples',
 	'Pi1',
 	array('HelloWorld' => 'index'),
 	array()
 );
 
-Tx_Extbase_Utility_Plugin::registerTypoScript(
+Tx_Extbase_Utility_Plugin::configureDispatcher(
 	'MvcExtjsSamples',
 	'Pi2',
 	array('SimpleForm' => 'index,genres'),
 	array('SimpleForm' => 'genres')			// Action 'genres' is used for AJAX and thus should not be cached
 );
 
-Tx_Extbase_Utility_Plugin::registerTypoScript(
+Tx_Extbase_Utility_Plugin::configureDispatcher(
 	'MvcExtjsSamples',
 	'Pi3',
 	array('Twitter' => 'index'),
 	array()
 );
 
-Tx_Extbase_Utility_Plugin::registerTypoScript(
+Tx_Extbase_Utility_Plugin::configureDispatcher(
 	'MvcExtjsSamples',
 	'Pi4',
 	array('Feeds' => 'index,feeds'),
