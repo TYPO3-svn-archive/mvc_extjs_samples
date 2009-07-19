07efea7c48b8af7ac105796084905ba59305b15c
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-19 18:15:43 +0000

    Reorganized definitions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mvc_extjs_samples/trunk@22505 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,12 +3,19 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-	// Plugin registration (listing in backend)
+// ========== Plugin HelloWorld
+
 Tx_Extbase_Utility_Plugin::registerPlugin(
 	'MvcExtjsSamples',
 	'HelloWorld',
 	'MVC ExtJS Samples - Hello World'
 );
+
+	// Disable the display of layout, select_key and page fields
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['mvcextjssamples_helloworld'] = 'layout,select_key,pages';
+
+
+// ========== Plugin SimpleForm
 
 Tx_Extbase_Utility_Plugin::registerPlugin(
 	'MvcExtjsSamples',
@@ -16,11 +23,27 @@ Tx_Extbase_Utility_Plugin::registerPlugin(
 	'MVC ExtJS Samples - Simple Form'
 );
 
+	// Disable the display of layout, select_key and page fields
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['mvcextjssamples_simpleform'] = 'layout,select_key,pages';
+
+
+// ========== Plugin Twitter
+
 Tx_Extbase_Utility_Plugin::registerPlugin(
 	'MvcExtjsSamples',
 	'Twitter',
 	'MVC ExtJS Samples - Twitter'
 );
+
+	// Disable the display of layout, select_key and page fields
+$TCA['tt_content']['types']['list']['subtypes_excludelist']['mvcextjssamples_twitter'] = 'layout,select_key,pages';
+
+	// Register FlexForm
+$TCA['tt_content']['types']['list']['subtypes_addlist']['mvcextjssamples_twitter'] = 'pi_flexform';
+t3lib_extMgm::addPiFlexFormValue('mvcextjssamples_twitter', 'FILE:EXT:mvc_extjs_samples/Configuration/FlexForms/Twitter.xml');
+
+
+// ========== Plugin Feeds
 
 Tx_Extbase_Utility_Plugin::registerPlugin(
 	'MvcExtjsSamples',
@@ -29,34 +52,14 @@ Tx_Extbase_Utility_Plugin::registerPlugin(
 );
 
 	// Disable the display of layout, select_key and page fields
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['mvcextjssamples_helloworld'] = 'layout,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['mvcextjssamples_simpleform'] = 'layout,select_key,pages';
-
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['mvcextjssamples_twitter'] = 'layout,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist']['mvcextjssamples_twitter'] = 'pi_flexform';
-t3lib_extMgm::addPiFlexFormValue('mvcextjssamples_twitter', 'FILE:EXT:mvc_extjs_samples/Configuration/FlexForms/Twitter.xml');
-
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['mvcextjssamples_feeds'] = 'layout,select_key,pages';
+
+	// Register FlexForm
 $TCA['tt_content']['types']['list']['subtypes_addlist']['mvcextjssamples_feeds'] = 'pi_flexform';
 t3lib_extMgm::addPiFlexFormValue('mvcextjssamples_feeds', 'FILE:EXT:mvc_extjs_samples/Configuration/FlexForms/Feeds.xml');
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Mvc ExtJS Samples');   
 
-	// TCA definition
-t3lib_extMgm::allowTableOnStandardPages('tx_mvcextjssamples_domain_model_movie');
-$TCA['tx_mvcextjssamples_domain_model_movie'] = array(
-    'ctrl' => array(
-        'title'     => 'LLL:EXT:mvc_extjs_samples/Resources/Private/Language/locallang_db.xml:tx_mvcextjssamples_domain_model_movie',        
-        'label'     => 'title',    
-        'tstamp'    => 'tstamp',
-        'crdate'    => 'crdate',
-        'cruser_id' => 'cruser_id',
-        'default_sortby' => 'ORDER BY title',    
-        'enablecolumns' => array(
-            'disabled' => 'hidden',
-        ),
-        'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/tca.php',
-		'iconfile' 			=> t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/icon_tx_mvcextjssamples_domain_model_movie.gif',
-    ),
-);
+// ========== Common
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'MVC ExtJS Samples');   
 ?>
