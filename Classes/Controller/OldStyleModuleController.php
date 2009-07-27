cc083d5f29c85713e5b7d714c6f62147a60f4731
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-27 16:41:59 +0000

    Removed ExtJS dependency as it is useless for plain-old backend modules
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mvc_extjs_samples/trunk@22819 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/OldStyleModuleController.php
+++ Classes/Controller/OldStyleModuleController.php
@@ -32,7 +32,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html
  * @version     SVN: $Id: BlankModuleController.php 22770 2009-07-25 15:32:38Z xperseguers $
  */
-class Tx_MvcExtjsSamples_Controller_OldStyleModuleController extends Tx_MvcExtjsSamples_ExtJS_Controller_ActionController {
+class Tx_MvcExtjsSamples_Controller_OldStyleModuleController extends Tx_Extbase_MVC_Controller_ActionController {
 	
 	/**
 	 * Holds reference to the template class
