ee0cb3b0a3511f4130b7778f30141f10869c3175
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-24 12:51:29 +0000

    Reflected changes in EXT:mvc_extjs
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mvc_extjs_samples/trunk@25792 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/SimpleModuleFunctionController.php
+++ Classes/Controller/SimpleModuleFunctionController.php
@@ -32,7 +32,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html
  * @version     SVN: $Id$
  */
-class Tx_MvcExtjsSamples_Controller_SimpleModuleFunctionController extends Tx_MvcExtjs_ExtJS_Controller_FunctionActionController {
+class Tx_MvcExtjsSamples_Controller_SimpleModuleFunctionController extends Tx_MvcExtjs_ExtJS_Controller_ExtensionObjectActionController {
 
 	/**
 	 * Initializes the menu entries.
